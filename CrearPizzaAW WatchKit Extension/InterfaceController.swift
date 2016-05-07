6fa7fd33ac43fd1f44f99753c9d39f7e9bfa1ebf
Avril  Hernández <avril@macintosh.local>
2016-05-06 22:08:40 -0500

    Initial Commit

---
+++ CrearPizzaAW WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  CrearPizzaAW WatchKit Extension
+//
+//  Created by Avril  Hernández on 06/05/16.
+//  Copyright © 2016 AHB. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ CrearPizzaAW/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrearPizzaAW
+//
+//  Created by Avril  Hernández on 06/05/16.
+//  Copyright © 2016 AHB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
